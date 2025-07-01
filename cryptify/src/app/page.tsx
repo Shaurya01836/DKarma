9e4909360fbbfb58f45d672d5e3779e76f58b557
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-07-01 23:44:52 +0530

    Initial commit
    Project scaffolding and initial file setup.

--- cryptify/src/app/page.tsx
+++ cryptify/src/app/page.tsx
@@ -1,5 +1,10 @@
+import { BentoGridSecondDemo } from "@/components/layout/BentoGridSecondDemo";
+import { CardHoverEffectDemo } from "@/components/layout/CardHoverEffectDemo";
 import { HeroSection } from "@/components/layout/HeroSection";
 import { NavbarDemo } from "@/components/layout/NavbarDemo";
+import PricingCards from "@/components/layout/PricingCards";
+import { StickyScrollRevealDemo } from "@/components/layout/StickyScrollRevealDemo";
+import { Stick } from "next/font/google";
 
 import React from "react";
 
@@ -8,6 +13,10 @@ function page() {
     <div className="bg-background text-foreground ">
       <NavbarDemo />
       <HeroSection />
+      <CardHoverEffectDemo />
+      <StickyScrollRevealDemo />
+      <BentoGridSecondDemo />
+      <PricingCards />
     </div>
   );
 }
